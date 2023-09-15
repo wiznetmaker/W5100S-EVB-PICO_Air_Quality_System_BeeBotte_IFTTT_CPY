ebcc1c8a482c9366a3eb041c18219722e024b490
markyu <markyu@wiznet.hk>
2023-09-15 15:01:40 +0800

    Add files via upload

---
+++ secrets.py
@@ -0,0 +1,12 @@
+# SPDX-FileCopyrightText: 2021 ladyada for Adafruit Industries
+# SPDX-License-Identifier: MIT
+
+# This file is where you keep secret settings, passwords, and tokens!
+# If you put them in the code you risk committing that info or sharing it
+
+secrets = {
+
+    "Beebotte_channel": "AQS",
+    "Beebotte_user": "token_XXXXXXXXXXXXXXXXXXXXXXXX",
+
+}
